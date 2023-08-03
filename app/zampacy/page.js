76c45cabbbbe76e1f583e5404c13ccdc38e14849
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-08-03 18:27:06 +0200

    Removed un-necessary code
    [skip ci]

--- app/zampacy/page.js
+++ app/zampacy/page.js
@@ -1,11 +1,13 @@
 import "../styles/zampacy.css"
-import ElementLoop from "../lib/elementLoop.js"
+import Layout from "../layoutMenu.js"
 
 export default function Zampacy() {
     return(
         <div>
+            <Layout>
+            </Layout>
             <h1 id="title">NOI TENIAMO DATI<br />VOI TENETE NIENTE</h1>
-            <ElementLoop count={10} />
+            <div id="empty"></div>
             <text id="foot-print">Per motivi legali dobbiamo seguire i Comandamenti del Zampanesimo</text>
         </div>
     )
