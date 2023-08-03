7ddbd51b7f0b4fbfd1cedef35ee258ad269151e7
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-08-03 18:41:52 +0200

    Added concorrenti

---
+++ app/mastirchof-25/concorrenti/page.js
@@ -0,0 +1,16 @@
+import Layout from '../../layoutMenu';
+import "../../styles/concorrenti.css"
+
+export const metadata = {
+    title: "I Concorrenti di Mastirchof 25",
+};
+
+export default function Concorrenti() {
+    return(
+        <div>
+            <Layout>
+            </Layout>
+            <h1 id="header" className='center'>Al momento non abbiamo i risultati<br />Tornare più tardi (1/2 mesi)</h1>
+        </div>
+    )
+}
