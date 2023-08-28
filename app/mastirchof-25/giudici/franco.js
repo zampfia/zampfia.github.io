bf784426e7600bdc5816510360eb9d9b6ea356f6
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-08-28 23:50:31 +0200

    "Fixed" images not aligned part 2

--- app/mastirchof-25/giudici/franco.js
+++ app/mastirchof-25/giudici/franco.js
@@ -7,7 +7,7 @@ const Franco = () => {
       <Image id="image" className="rightimage" src="/photos/franco.png" width={500} height={300} alt="Giudice Franco"/>
       <div className='rightchilddiv'>
         <h4 id="header">Il Giudice <i>Franco</i></h4>
-        <text id='text'><span style={{color:"white"}}>FrancoFrancoFrancoFrancoFrancoFraj</span><i>Franco</i></text>
+        <text id='text'><span style={{color:"white"}}>FrancoFrancoFrancoFrancoFrancoFrancoFrancoFra</span><i>Franco</i></text>
       </div>
     </div>
   ) 
