1e6f0fb3bce4911c03d4457fc6b6266fce3d26c6
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-08-30 01:40:20 +0200

    Moved every file (in theory) to 4 space tab

--- app/mastirchof-25/giudici/franco.js
+++ app/mastirchof-25/giudici/franco.js
@@ -2,15 +2,15 @@ import "../../styles/giudici.css"
 import Image from 'next/image';
 
 const Franco = () => {  
-  return (
-    <div className='flexdiv right'>
-      <Image id="image" className="rightimage" src="/photos/franco.png" width={500} height={300} alt="Giudice Franco"/>
-      <div className='rightchilddiv'>
-        <h4 id="header">Il Giudice <i>Franco</i></h4>
-        <text id='text'><span style={{color:"white"}}>FrancoFrancoFrancoFrancoFrancoFrancoFrancoFra</span><i>Franco</i></text>
-      </div>
-    </div>
-  ) 
+    return (
+        <div className='flexdiv right'>
+            <Image id="image" className="rightimage" src="/photos/franco.png" width={500} height={300} alt="Giudice Franco"/>
+            <div className='rightchilddiv'>
+                <h4 id="header">Il Giudice <i>Franco</i></h4>
+                <text id='text'><span style={{color:"rgb(231, 237, 241)"}}>FrancoFrancoFrancoFrancoFrancoFrancoFrancoFra</span><i>Franco</i></text>
+            </div>
+        </div>
+    ) 
 }
 
 export default Franco;
