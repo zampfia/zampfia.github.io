6292d02a8e7155d80a75515d55a450b71817dd24
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-08-29 23:36:13 +0200

    Added a close panel
    [skip ci]

--- app/sideMenu.js
+++ app/sideMenu.js
@@ -4,16 +4,21 @@ import Link from 'next/link';
 
 const SideMenu = ({ isOpen, onClose }) => {
     return (
-        <div className={`side-menu ${isOpen ? 'open' : ''}`}>
-            <button className={"close-button"} onClick={onClose}></button>
-            <ul>
-                <li>
-                    <Link className="link" href="/">Home Page<br /></Link>
-                </li>
-                <li>
-                    <Link className="link" href="/about">Chi siamo</Link>
-                </li>
-            </ul>
+        <div>
+            <div>
+                <button className={`close-panel ${isOpen ? 'open' : ''}`} onClick={onClose}></button>
+            </div>
+            <div className={`side-menu ${isOpen ? 'open' : ''}`}>
+                <button className={"close-button"} onClick={onClose}></button>
+                <ul>
+                    <li>
+                        <Link className="link" href="/">Home Page<br /></Link>
+                    </li>
+                    <li>
+                        <Link className="link" href="/about">Chi siamo</Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 };
