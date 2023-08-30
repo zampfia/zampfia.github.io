1537fb1bb3dafcfe3cc875901ab492d25800f66f
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-08-30 16:36:52 +0200

    Fixed margins and order

--- app/mastirchof-25/giudici/franco.js
+++ app/mastirchof-25/giudici/franco.js
@@ -4,11 +4,11 @@ import Image from 'next/image';
 const Franco = () => {  
     return (
         <div className='flexdiv right'>
-            <Image id="image" className="rightimage" src="/photos/franco.png" width={500} height={300} alt="Giudice Franco"/>
-            <div className='rightchilddiv'>
+            <div className='rightchilddiv francochilddiv'>
                 <h4 id="header">Il Giudice <i>Franco</i></h4>
-                <text id='text'><span style={{color:"rgb(231, 237, 241)"}}>FrancoFrancoFrancoFrancoFrancoFrancoFrancoFra</span><i>Franco</i></text>
+                <text id='text'><i>Franco</i></text>
             </div>
+            <Image id="image" className="rightimage" src="/photos/franco.png" width={500} height={300} alt="Giudice Franco"/>
         </div>
     ) 
 }
--- app/mastirchof-25/giudici/gigi.js
+++ app/mastirchof-25/giudici/gigi.js
@@ -4,11 +4,11 @@ import Image from 'next/image';
 const Gigi = () => {  
     return (
         <div className='flexdiv right'>
-            <Image id="image" className="rightimage" src="/photos/gigi.png" width={500} height={300} alt="Giudice Gigi"/>
             <div className='rightchilddiv'>
                 <h4 id="header">Il Giudice <i>Gigi</i></h4>
                 <text id='text'>Fratello del Giudice Zampa. Rinomato nel mondo per la grande intelligenza culinaria</text>
             </div>
+            <Image id="image" className="rightimage" src="/photos/gigi.png" width={500} height={300} alt="Giudice Gigi"/>
         </div>
     ) 
 }
