ceda38995c75ea4ec57d8b28f3b177779d4b2fbc
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-08-30 17:58:16 +0200

    Doing stuff
    [skip ci]

--- app/page.js
+++ app/page.js
@@ -1,27 +1,38 @@
+"use client";
+
 import dynamic from 'next/dynamic';
 import "./styles/page.css";
 import ScrollingImages from './scrollingImages';
 import Layout from './layoutMenu';
+import { useState } from "react";
 
 const ImageContainer = dynamic(() => import('./imgContainer'), {
-  ssr: false,
+    ssr: false,
 });
 
 export const metadata = {
-  title: "Zampa è bravo",
+    title: "Zampa è bravo",
 };
 
-const HomePage = () => (
-    <div>
-        <Layout>
-        </Layout>
-        <div className="margin">
-            <h1 className="title">ZAMPA E' IL PIU' GRANDE</h1>
-            <ImageContainer />
-            <ScrollingImages />
-        </div>
-    </div>
-);
+export default function HomePage() {
+    const [isUnicStyle, setIsUnicStyle] = useState();
 
-export default HomePage;
+    const handleStyleClick = () => {
+        setIsUnicStyle(!isUnicStyle);
+    };
+
+	return (
+        <div>
+            <Layout>
+            </Layout>
+            <div className="margin">
+                <h1 className="title">ZAMPA E' IL PIU' GRANDE</h1>
+                <ImageContainer />
+                <ScrollingImages />
+                <button onClick={handleStyleClick}>Attiva lo stile <i>unico</i></button>
+                <text>{isUnicStyle ? "Unic" : ""}</text>
+            </div>
+        </div>
+    );
+};
 
