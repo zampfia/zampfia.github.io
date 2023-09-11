9d737d4d13d71e773f240d1a8dc1532cb0cd7794
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-09-11 20:26:14 +0200

    Added privacy policy (obviously made by a self-referered stupid as of yesterday)

---
+++ app/privacy/page.tsx
@@ -0,0 +1,41 @@
+import Layout from "../layoutMenu";
+import "../styles/privacy.css"
+
+export default function Privacy() {
+    return(
+        <div>
+            <Layout />
+            <div className="margin">
+                <h1 id="title">Privacy Policy</h1>
+                <div className="childdiv">
+                    <h3>Che dati otteniamo?</h3>
+                    <text>Otteniamo le seguenti informazioni:</text>
+                    <ul>
+                        <li>Informazioni identificativi (Nome, email, etc..)</li>
+                    </ul>
+                    <h3>Per cosa usiamo i tuoi dati?</h3>
+                    <text>Usiamo i tuoi dati per:</text>
+                    <ul>
+                        <li>Fornire i nostri servizi</li>
+                        <li>Offrire servizi personalizzati</li>
+                    </ul>
+                    <h3>Come otteniamo i tuoi dati?</h3>
+                    <text>Otteniamo i tuoi dati attreverso:</text>
+                    <ul>
+                        <li>Moduli inviati da te</li>
+                    </ul>
+                    <h3>Dove conserviamo i tuoi dati?</h3>
+                    <text>Vengono conservati in database offerti da <a href="https://www.mongodb.com/atlas">MongoDB Atlas</a></text>
+                    <h3>A che cosa hai diritto di fare?</h3>
+                    <text>Ogni utente ha concesso i seguenti diritti sui propri dati:</text>
+                    <ul>
+                        <li>Diritto di accedere ai propri dati</li>
+                        <li>Diritto di modificare i propri dati</li>
+                        <li>Diritto di cancellare i propri dati</li>
+                    </ul>
+                    <text>Per qualasiasi richiesta inviare email a: <a href="mailto:legal@zampfia.onmicrosoft.com">legal@zampfia.onmicrosoft.com</a></text>
+                </div>
+            </div>
+        </div>
+    )
+}
