6c30f4dda7dbfafd0bba6b599d2b8529c439a342
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-10-12 18:55:00 +0200

    Forgot Form

--- app/form/page.tsx
+++
@@ -1,9 +0,0 @@
-import ZampaForm from "../components/zampaForm";
-
-export default function ZampaFormPage() {
-    return (
-        <div className="margin">
-            <ZampaForm />
-        </div>
-    );
-}
