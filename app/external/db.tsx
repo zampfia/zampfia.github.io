9ec860aa8ad51169190192975d5e315ce493e441
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-10-12 21:22:35 +0200

    Changed to URI beacuse of MongoDB Atlas integration with Vercel

--- app/external/db.tsx
+++ app/external/db.tsx
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_CONNECT;
+const uri = process.env.MONGODB_URI;
 
 export async function insertToDB<T>(db: string, collect: string, doc: T) {
     const client = new MongoClient(uri);
