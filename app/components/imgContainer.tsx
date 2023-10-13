60640b2da7932eeb95c0488d1efee9d90a47af96
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-10-13 22:50:16 +0200

    Remove unused showingFrame

--- app/components/imgContainer.tsx
+++ app/components/imgContainer.tsx
@@ -15,8 +15,6 @@ const getRandomImage = () => {
     return images[randomIndex];
 };
 
-export var showingFrame;
-
 const ImageContainer = () => {
     const [clickCount, setClickCount] = useState(0);
     const [randomImage, setRandomImage] = useState(null);
@@ -26,7 +24,6 @@ const ImageContainer = () => {
     useEffect(() => {
         if (clickCount === 30) {
             setShowFrame(true);
-            showingFrame = true;
             setClickCount(0);
             setZampaCount(zampaCount + 1);
             setRandomImage(null);
@@ -40,7 +37,6 @@ const ImageContainer = () => {
 
     const handleClose = () => {
         setShowFrame(false);
-        showingFrame = true;
     };
 
     return (
