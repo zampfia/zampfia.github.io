abd2834e8ca880aa0d6a5bf0445ab4c012cc4ad2
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-10-17 17:08:54 +0200

    Added getFromDB function in db.tsx
    Uses search index with one field
    [add]

--- app/external/db.tsx
+++ app/external/db.tsx
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+import { Document, MongoClient } from "mongodb";
 
 const uri = process.env.MONGODB_URI;
 
@@ -9,6 +9,41 @@ export async function insertToDB<T>(db: string, collect: string, doc: T) {
         const collection = database.collection(collect);
         const result = await collection.insertOne(doc);
         return result ? true : false;
+    } finally {
+        await client.close();
+    }
+}
+
+export async function getFromDB<T>(
+    db: string,
+    collect: string,
+    searchField: string,
+    fieldEntry: string,
+) {
+    const client = new MongoClient(uri);
+    try {
+        const database = client.db(db);
+        const collection = database.collection(collect);
+        const agg = [
+            {
+                $search: {
+                    text: {
+                        query: fieldEntry,
+                        path: searchField,
+                    },
+                },
+            },
+            {
+                $limit: 1,
+            },
+        ];
+        let cursor = collection.aggregate(agg);
+        var prevresult: Document;
+        await cursor.forEach(function (doc) {
+            prevresult = doc;
+        });
+        const result = prevresult;
+        return result as T;
     } finally {
         await client.close();
     }
