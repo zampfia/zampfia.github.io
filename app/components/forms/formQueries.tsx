826880a0d41a63dbe1efe1649c657126d3518626
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-10-19 00:33:11 +0200

    Added password and min/max length in TextField in formQueries
    [add]

--- app/components/forms/formQueries.tsx
+++ app/components/forms/formQueries.tsx
@@ -9,6 +9,9 @@ export const TextField = ({
     classes = "",
     input_classes = "",
     required = false,
+    password = false,
+    min = 0,
+    max = Infinity,
 }) => {
     return (
         <div
@@ -24,12 +27,14 @@ export const TextField = ({
                 </span>
             </label>
             <input
-                type="text"
+                type={password ? "password" : "text"}
                 placeholder={placeholder}
                 className={`field input input-bordered w-full max-w-xs pl-2  ${input_classes}`}
                 id={id}
                 name={id}
                 required={required}
+                minLength={min}
+                maxLength={max}
             />
         </div>
     );
