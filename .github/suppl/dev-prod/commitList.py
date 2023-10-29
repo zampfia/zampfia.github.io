546331771c7ae796d9d4ce624e8ed5845cc1fde2
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-10-29 13:05:26 +0100

    Fixed properties actually working
    [no prod]
    [fix]

--- .github/suppl/dev-prod/commitList.py
+++ .github/suppl/dev-prod/commitList.py
@@ -3,18 +3,18 @@ class CommitList:
     def titles(self):
         return self._titles
     
-    @property
-    def hashes(self):
-        return self.hashes
-    
     @titles.setter
     def titles(self, value):
-        self._titles(value)
+        self._titles = value
+    
+    @property
+    def hashes(self):
+        return self._hashes
     
     @hashes.setter
     def hashes(self, value):
-        self._hashes(value)
+        self._hashes = value
     
-    def __init__(self) -> None:
-        self._titles = []
-        self._hashes = []
+    def __init__(self):
+        self._titles = list()
+        self._hashes = list()
