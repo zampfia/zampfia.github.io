d28ec27c71c7cdf6825637179f0a1f697ce49e4d
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-11-21 21:58:37 +0100

    Added admin lock to wallet page
    [add]
    [dev->prod]
    Title: Switched db to Prisma & Added admin system & Added email support & more

--- app/wallet/page.tsx
+++ app/wallet/page.tsx
@@ -1,4 +1,45 @@
-export default function Charity() {
+"use client";
+
+import { useCookies } from "next-client-cookies";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function WalletPage() {
+    const cookies = useCookies();
+    if (cookies.get("token") === undefined) {
+        useRouter().push("/auth?redirect=/wallet&name=Magico Portafoglio");
+    }
+    useEffect(() => {
+        getUser();
+    }, []);
+
+    const getUser = async () => {
+        var userRes: Response;
+        await fetch("/api/admin/auth/authenticate?perms=experimental&", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        }).then((res) => (userRes = res));
+        if (userRes.status === 404) {
+            alert("TUO TOKEN SU TUO BROWSER NON ESSERE IN NOSTRO DATABASE");
+            cookies.remove("token");
+            window.location.reload();
+        } else if (userRes.status === 403) {
+            alert("TU NON AVERE PERMESSI. BUTTARTI FUORI IO");
+            cookies.remove("token");
+            window.location.reload();
+        } else if (userRes.status === 500) {
+            alert(
+                "ESSERCI PROBLEMI! CONTATTARE ASSISTENZA TECNICA VIA POSTA ALL'INDIRIZZO Longyearbyen 9170, Svalbard e Jan Mayen, Norvegia!",
+            );
+        } else if (userRes.status != 200) {
+            alert(
+                "ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ALLARME ",
+            );
+        }
+    };
+
     return (
         <div className="margin">
             <h1>Add money</h1>
