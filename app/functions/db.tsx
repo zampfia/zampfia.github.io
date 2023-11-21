a4bf10f5d6ca52c24210b1c9fea266f4e0aeb1cc
gdar463 <63969957+gdar463@users.noreply.github.com>
2023-11-21 21:51:15 +0100

    Changed db interface to Prisma
    [change]

--- app/functions/db.tsx
+++ app/functions/db.tsx
@@ -1,62 +1,75 @@
-import { Document, MongoClient } from "mongodb";
+import { PrismaClient } from "@prisma/client";
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const uri = process.env.MONGODB_URI;
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-export async function insertToDB<T>(db: string, collect: string, doc: T) {
-    const client = new MongoClient(uri);
-    try {
-        const database = client.db(db);
-        const collection = database.collection(collect);
-        const result = await collection.insertOne(doc);
-        return result ? true : false;
-    } finally {
-        await client.close();
-    }
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
+
+export enum Forms {
+    CONTACTING = "contacting",
+    ZAMPAVINTI = "zampaVinti",
 }
 
-export async function getFromDB<T>(
-    db: string,
-    collect: string,
-    searchField: string,
-    fieldEntry: string,
-) {
-    const client = new MongoClient(uri);
-    try {
-        const database = client.db(db);
-        const collection = database.collection(collect);
-        const agg = [
-            {
-                $search: {
-                    text: {
-                        query: fieldEntry,
-                        path: searchField,
-                    },
-                },
-            },
-            {
-                $limit: 1,
-            },
-        ];
-        let cursor = collection.aggregate(agg);
-        var prevresult: Document;
-        await cursor.forEach(function (doc) {
-            prevresult = doc;
-        });
-        const result = prevresult;
-        return result as T;
-    } finally {
-        await client.close();
-    }
-}
+// const uri = process.env.MONGODB_URI;
 
-export async function getDocCountFromDB(db: string, collect: string) {
-    const client = new MongoClient(uri);
-    try {
-        const database = client.db(db);
-        const collection = database.collection(collect);
-        const result = await collection.countDocuments();
-        return result;
-    } finally {
-        await client.close();
-    }
-}
+// export async function insertToDB<T>(collect: , doc: T) {
+// const collection = await prisma.
+
+// const client = new MongoClient(uri);
+// try {
+//     const database = client.db(db);
+//     const collection = database.collection(collect);
+//     const result = await collection.insertOne(doc);
+//     return result ? true : false;
+// } finally {
+//     await client.close();
+// }
+// }
+
+// export async function getFromDB<T>(
+//     collect: string,
+//     searchField: string,
+//     fieldEntry: string,
+// ) {
+//     const client = new MongoClient(uri);
+//     try {
+//         const database = client.db(db);
+//         const collection = database.collection(collect);
+//         const agg = [
+//             {
+//                 $search: {
+//                     text: {
+//                         query: fieldEntry,
+//                         path: searchField,
+//                     },
+//                 },
+//             },
+//             {
+//                 $limit: 1,
+//             },
+//         ];
+//         let cursor = collection.aggregate(agg);
+//         var prevresult: Document;
+//         await cursor.forEach(function (doc) {
+//             prevresult = doc;
+//         });
+//         const result = prevresult;
+//         return result as T;
+//     } finally {
+//         await client.close();
+//     }
+// }
+
+// export async function getDocCountFromDB(collect: string) {
+//     const client = new MongoClient(uri);
+//     try {
+//         const database = client.db(db);
+//         const collection = database.collection(collect);
+//         const result = await collection.countDocuments();
+//         return result;
+//     } finally {
+//         await client.close();
+//     }
+// }
